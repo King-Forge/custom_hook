de3d9c91db1a580fa087fc2197a1375f78d3f7b5
King-Forge <jak1456@gmail.com>
2026-02-12 11:41:13 -0500

    added abort controller and test for HTTP errors because lab code was fragile, probably unnecessary but it's good practice

--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,5 +1,33 @@
-const UseFetch = (url) => {
-    
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+    //todo: add abortController
+    const[data, setData] = useState(null);
+
+    useEffect( () => {
+        setData(null); //just in case this aborts or errors out
+        //register new abortController, pass .signal to fetch, abort on cleanup
+        const controller = new AbortController();
+
+        fetch(url, {signal: controller.signal })
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error(`HTTP error: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((data) => setData(data))
+        .catch((e) => {
+            if (e.name !== "AbortError") {
+                console.log(`Error fetching data: ${e.message}`);
+            }
+        });
+
+        return () => controller.abort();
+
+    },[url])
+
+    return [data];
 }
 
-export default UseFetch
+export default useFetch
